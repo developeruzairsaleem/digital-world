794a2869e1eb3f125fc94ab0673c99b337c59262
developeruzairsaleem <uzairsaleemdev@gmail.com>
2025-03-25 08:22:17 -0700

    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  siteUrl: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3002", // Replace with your site's URL
+  generateRobotsTxt: true, // (Optional) Generate a robots.txt file
+  // optional: additional configuration options can be added here
+};
